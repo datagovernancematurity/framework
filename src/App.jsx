8aaee17c8954b3078507e9daf6ef72e1fda15951
datagovernancematurity <datagovernancematuritymodel@gmail.com>
2025-04-22 13:36:36 -0300

    Page layout implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Main from './pages/Main'
+import Introduction from './pages/Introduction'
+import Form from './pages/Form'
+import Level from './pages/Level'
+import DimensionsAndLevels from './pages/DimensionsAndLevels'
+import Aspects from './pages/Aspects'
+import Roles from './pages/Roles'
+import AboutTheAuthors from './pages/AboutTheAuthors'
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <header>
+        <nav className="navbar">
+          <h1>DGM2 Framework</h1>
+          <input type="checkbox" id="menu-toggle" />
+          <label htmlFor="menu-toggle" className="menu-icon">&#9776;</label>
+          <ul className="nav-links">
+            <li><Link to="/framework">Main</Link></li>
+            <li><Link to="/introduction">Introduction</Link></li>
+            <li><Link to="/roles">Roles</Link></li>
+            <li><Link to="/level">Levels</Link></li>
+            <li><Link to="/aspects">Aspects</Link></li>
+            <li><Link to="/dimensions">Dimensions</Link></li>
+            <li><Link to="/form">Assess the maturity</Link></li>
+            <li><Link to="/authors">Authors</Link></li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <Routes>
+          <Route path="/framework" element={<Main />} />
+          <Route path="/introduction" element={<Introduction />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/level" element={<Level />} />
+          <Route path="/dimensions" element={<DimensionsAndLevels />} />
+          <Route path="/aspects" element={<Aspects />} />
+          <Route path="/roles" element={<Roles />} />
+          <Route path="/authors" element={<AboutTheAuthors />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
-
-export default App
